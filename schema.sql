b53b4ee7520cc16eeb26efea907cf8ba66691379
Baptlacast <128820378+baptlacast@users.noreply.github.com>
2024-03-20 15:32:30 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE clients (
     prenom TEXT NOT NULL,
     adresse TEXT NOT NULL
 );
+
+
+DROP TABLE IF EXISTS livres;
+CREATE TABLE livres (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    titre TEXT NOT NULL,
+    auteur TEXT NOT NULL,
+);
